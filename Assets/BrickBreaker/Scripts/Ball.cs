1894edfb82a78b07d06733f8ae5c4e329c456c70
LOLO\loric <loric.ravassard@laposte.net>
2022-06-01 00:16:28 +0200

    Correction bounce brick breaker

--- Assets/BrickBreaker/Scripts/Ball.cs
+++ Assets/BrickBreaker/Scripts/Ball.cs
@@ -10,7 +10,7 @@ public class Ball : MonoBehaviour
     const int PUISSANCE_DEVIATION = 3;
     const float BALL_Y = -4f;
     const float GRAVITY = 0.05f;
-    protected Rigidbody2D rigidbody;
+    protected Rigidbody2D rigidBody;
     [HideInInspector] public Master master_Script;
     protected AudioSource sound;
     [SerializeField] protected AudioClip[] clip;
@@ -18,7 +18,7 @@ public class Ball : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
         sound = GetComponent<AudioSource>();
         StartCoroutine(Spawn());
     }
@@ -26,14 +26,20 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(rigidBody.velocity);
         if(transform.position.y <= BOTTOM_SCREEN)
         {
             sound.clip = clip[2];
             sound.Play();
-            int newScore = master_Script.score - 50;
+            int newScore = master_Script.score - 500;
             if (newScore >= 0)
             {
                 master_Script.score = newScore;
+                master_Script.displayedScore.SetText("Score : " + master_Script.score);
+            }
+            else
+            {
+                master_Script.score = 0;
                 master_Script.displayedScore.SetText("Score : " + master_Script.score);
             }
             
@@ -46,12 +52,12 @@ public class Ball : MonoBehaviour
         sound.clip = clip[3];
         sound.Play();
         transform.position = new Vector3(0, BALL_Y, 0);
-        rigidbody.velocity = new Vector2(0, 0);
-        rigidbody.gravityScale = 0; //Pour que la balle ne tombe pas à cause de la gravité
+        rigidBody.velocity = new Vector2(0, 0);
+        rigidBody.gravityScale = 0; //Pour que la balle ne tombe pas à cause de la gravité
         yield return new WaitForSeconds(TIME_SPAWN);
-        rigidbody.gravityScale = GRAVITY; //On réactive la gravité pour éviter que la balle se bloque en Y
+        rigidBody.gravityScale = GRAVITY; //On réactive la gravité pour éviter que la balle se bloque en Y
                                           //La valeur de la gravité est faible pour ne pas influencer la vitesse de la balle
-        rigidbody.velocity = new Vector2(1, LAUNCH_SPEED);
+        rigidBody.velocity = new Vector2(1, LAUNCH_SPEED);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -63,9 +69,8 @@ public class Ball : MonoBehaviour
             float ballPosition = transform.position.x;
             float paddlePosition = collision.transform.position.x;
             float diffX;
-            //diffX = paddlePosition - ballPosition;
             diffX = ballPosition - paddlePosition;
-            rigidbody.velocity += new Vector2(diffX * PUISSANCE_DEVIATION, 0);
+            rigidBody.velocity += new Vector2(diffX * PUISSANCE_DEVIATION, 0);
         }
         
         if(collision.gameObject.tag == "wall")
